d894361d538d35318fbe7fdb21f2ad4302c40fd3
Shamim742 <shamimislam2742@gmail.com>
2023-02-15 08:58:49 +0600

    calculate phone price

--- js/case.js
+++ js/case.js
@@ -14,15 +14,19 @@ function updateCaseNumber(isIncrease) {
     return newCaseNumber;
 }
 
-document.getElementById('btn-case-plus').addEventListener('click', function () {
-    const newCaseNumber = updateCaseNumber(true);
-    
+function updateCaseTotalPrice(newCaseNumber){
     const caseTotalPrice = newCaseNumber * 59;
 
     const caseTotalElement = document.getElementById('case-total');
     caseTotalElement.innerText = caseTotalPrice;
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function () {
+    const newCaseNumber = updateCaseNumber(true);
+    updateCaseTotalPrice(newCaseNumber)
 })
 
 document.getElementById('btn-case-minus').addEventListener('click', function () {
-    updateCaseNumber();
+    const newCaseNumber = updateCaseNumber();
+    updateCaseTotalPrice(newCaseNumber);
 })
