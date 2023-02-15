7b9e1057a6431eb5069853d9c474d9f16c66acdc
Shamim742 <shamimislam2742@gmail.com>
2023-02-15 09:57:59 +0600

    utilities

--- js/case.js
+++ js/case.js
@@ -23,10 +23,12 @@ function updateCaseTotalPrice(newCaseNumber){
 
 document.getElementById('btn-case-plus').addEventListener('click', function () {
     const newCaseNumber = updateCaseNumber(true);
-    updateCaseTotalPrice(newCaseNumber)
+    updateCaseTotalPrice(newCaseNumber);
+    calculateSubTotal();
 })
 
 document.getElementById('btn-case-minus').addEventListener('click', function () {
     const newCaseNumber = updateCaseNumber();
     updateCaseTotalPrice(newCaseNumber);
+    calculateSubTotal();
 })
--- js/phone.js
+++ js/phone.js
@@ -26,10 +26,12 @@ document.getElementById('btn-phone-plus').addEventListener('click', function () 
     const newPhoneNumber =  updatePhoneNumber(true);
 
     updatePhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
 })
 
 document.getElementById('btn-phone-minus').addEventListener('click', function () {
-    const newPhoneNumber = updatePhoneNumber();
+    const newPhoneNumber = updatePhoneNumber(false);
 
     updatePhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
 })
